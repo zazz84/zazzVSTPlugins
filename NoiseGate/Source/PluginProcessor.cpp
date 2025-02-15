2af625dea2ddaaa9e6755c351813a61a96e534a8
zazz <filip.c@centrum.cz>
2025-02-15 14:55:10 +0100

    Added: NoiseGate

---
+++ NoiseGate/Source/PluginProcessor.cpp
@@ -0,0 +1,240 @@
+/*
+ * Copyright (C) 2025 Filip Cenzak (filip.c@centrum.cz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+
+const std::string NoiseGateAudioProcessor::paramsNames[] =     { "Threshold", "Hystersis", "Attack", "Hold", "Release", "Mix", "Volume" };
+const std::string NoiseGateAudioProcessor::paramsUnitNames[] = { " dB",       " dB",       " ms",    " ms",  " ms",     " %",  " dB" };
+
+//==============================================================================
+NoiseGateAudioProcessor::NoiseGateAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+	thresholdParameter = apvts.getRawParameterValue(paramsNames[0]);
+	hystersisParameter = apvts.getRawParameterValue(paramsNames[1]);
+	attackParameter    = apvts.getRawParameterValue(paramsNames[2]);
+	holdParameter      = apvts.getRawParameterValue(paramsNames[3]);
+	releaseParameter   = apvts.getRawParameterValue(paramsNames[4]);
+	mixParameter       = apvts.getRawParameterValue(paramsNames[5]);
+	volumeParameter    = apvts.getRawParameterValue(paramsNames[6]);
+}
+
+NoiseGateAudioProcessor::~NoiseGateAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String NoiseGateAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool NoiseGateAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool NoiseGateAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool NoiseGateAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double NoiseGateAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int NoiseGateAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int NoiseGateAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void NoiseGateAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String NoiseGateAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void NoiseGateAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void NoiseGateAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+	const int sr = (int)sampleRate;
+}
+
+void NoiseGateAudioProcessor::releaseResources()
+{
+	
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool NoiseGateAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void NoiseGateAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+	// Get params
+	const float thresholdbB = thresholdParameter->load();
+	const float hystersisdB = hystersisParameter->load();
+	const float attack = attackParameter->load();
+	const float hold = holdParameter->load();
+	const float release = releaseParameter->load();
+	const float mix = 0.01f * mixParameter->load();
+	const auto gain = juce::Decibels::decibelsToGain(volumeParameter->load());
+
+	// Mics constants
+	const auto channels = getTotalNumOutputChannels();
+	const auto samples = buffer.getNumSamples();
+
+	for (int channel = 0; channel < channels; channel++)
+	{
+		// Channel pointer
+		auto* channelBuffer = buffer.getWritePointer(channel);
+
+		auto& gate = m_gate[channel];
+		gate.set(attack, release, hold, thresholdbB, hystersisdB);
+
+		for (int sample = 0; sample < samples; sample++)
+		{
+			// Read
+			const float in = channelBuffer[sample];
+
+			// Process
+			const float out = gate.process(in);
+		
+			//Out
+			channelBuffer[sample] = (1.0f - mix) * in + mix * out;
+		}
+	}
+
+	buffer.applyGain(gain);
+}
+
+//==============================================================================
+bool NoiseGateAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* NoiseGateAudioProcessor::createEditor()
+{
+    return new NoiseGateAudioProcessorEditor (*this, apvts);
+}
+
+//==============================================================================
+void NoiseGateAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{	
+	auto state = apvts.copyState();
+	std::unique_ptr<juce::XmlElement> xml(state.createXml());
+	copyXmlToBinary(*xml, destData);
+}
+
+void NoiseGateAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+	std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+
+	if (xmlState.get() != nullptr)
+		if (xmlState->hasTagName(apvts.state.getType()))
+			apvts.replaceState(juce::ValueTree::fromXml(*xmlState));
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout NoiseGateAudioProcessor::createParameterLayout()
+{
+	APVTS::ParameterLayout layout;
+
+	using namespace juce;
+
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[0], paramsNames[0], NormalisableRange<float>( -60.0f,   0.0f,  1.0f, 1.0f),   0.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[1], paramsNames[1], NormalisableRange<float>(   0.0f,  18.0f,  1.0f, 1.0f),   0.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[2], paramsNames[2], NormalisableRange<float>(   0.0f, 400.0f, 0.05f, 0.4f),  10.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[3], paramsNames[3], NormalisableRange<float>(   0.0f, 400.0f, 0.05f, 0.5f),   0.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[4], paramsNames[4], NormalisableRange<float>(   0.0f, 400.0f, 0.05f, 0.4f), 100.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[5], paramsNames[5], NormalisableRange<float>(   0.0f, 100.0f,  1.0f, 1.0f), 100.0f));
+	layout.add(std::make_unique<juce::AudioParameterFloat>(paramsNames[6], paramsNames[6], NormalisableRange<float>( -18.0f,  18.0f,  0.1f, 1.0f),   0.0f));
+
+	return layout;
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new NoiseGateAudioProcessor();
+}
